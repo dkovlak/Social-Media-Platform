0c6fa3856579dd61020b301c381dd6bc58d4604d
David Kovlak <kovlakdavid4@gmail.com>
2023-09-27 18:47:06 -0700

    class Post

--- Social Media Platform/Post.cs
+++ Social Media Platform/Post.cs
@@ -1,6 +1,29 @@
-﻿namespace Social_Media_Platform
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Social_Media_Platform
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User Author { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+
+        public int Likes { get; set; } = 0;
+        public int Dislikes { get; set; } = 0;
+
+        public string Location { get; set; }
     }
 }
